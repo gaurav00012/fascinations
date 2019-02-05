59bd98912e49a7ebc02fbb443a4ed6efeb461053
gaurav00012 <aggarwal.gaurav611@gmail.com>
2019-02-05 23:18:15 +0530

    first commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+
+
+class UserController extends Controller
+{
+    //
+
+    public function register(Request $request){
+
+    	echo "i am register";
+    }
+}
